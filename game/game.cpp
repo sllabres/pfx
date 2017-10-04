b654227c1bbee9669ed6df2aca990a882c384267
Sebastian Llabres <sllabres@gmail.com>
2017-10-04 19:04:21 +0100

    walking animation

--- game/game.cpp
+++ game/game.cpp
@@ -3,38 +3,36 @@
 void Game::LoadResources()
 {
 	Display* display = FX->video.GetDisplay(0);
-	/*spritePlane = new SpritePlane(display);
+	spritePlane = new SpritePlane(display);
 	spritePlane->draworder = SpritePlaneDrawOrder::YPOSITION;
+
+	/*
 	background = new Sprite(new Bitmap("resources/background-main.bmp"));
 	background->position.x = display->gameresolution.w / 2;
 	background->position.y = display->gameresolution.h / 2;
 	background->draworigin.x = background->GetDimensions().w / 2;
-	background->draworigin.y = background->GetDimensions().h / 2;	
-	spritePlane->sprites.Add(background);*/
-	
-	
+	background->draworigin.y = background->GetDimensions().h / 2;
+	spritePlane->sprites.Add(background);
+	tileMap = new TilemapPlane(display, tiles, tileSize, mapSize);
+	tileMap->index0isempty = false;
+	tileMap->animations.Add(animation);
+	*/
 
-	Atlas* tiles = new Atlas(new Bitmap("resources/barbarian-walk.bmp"));		
-	Size<int> tileSize(36, 63);	
-	Size<int> mapSize(144, 63);
+
+	Size<int> tileSize(36, 63);
+	Atlas* tiles = new Atlas(new Bitmap("resources/walk.png"), tileSize);	
 	animation = new Animation();	
-	animation->Start();
-	animation->frames.Add(new AnimationFrame(0, 1));
-	animation->frames.Add(new AnimationFrame(1, 1));
-	animation->frames.Add(new AnimationFrame(2, 1));
-	animation->frames.Add(new AnimationFrame(3, 1));
-	tileMap = new TilemapPlane(display, tiles, tileSize, mapSize);	
-	tileMap->alpha = 255;	
-	tileMap->animations.Add(animation);				
-	
-	
-	/*AnimatedSprite* as = new AnimatedSprite(tiles, a);			
-	spritePlane->sprites.Add(as);*/
+	//animation->loopanimation = false;
+	animation->frames.Add(new AnimationFrame(0, 10));
+	animation->frames.Add(new AnimationFrame(1, 10));
+	animation->frames.Add(new AnimationFrame(2, 10));
+	animation->frames.Add(new AnimationFrame(3, 10));			
+	spritePlane->sprites.Add(new AnimatedSprite(tiles, animation));
 }
 
 void Game::Start()
 {
-	
+
 }
 
 void Game::Pause()
@@ -54,5 +52,5 @@ void Game::EventOccured(Event* What)
 }
 
 void Game::Update()
-{		
+{
 }
